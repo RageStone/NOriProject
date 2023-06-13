f12c05ec550abe8aba7b2783b0c46d501969fb7b
RageStone <62663929+RageStone@users.noreply.github.com>
2023-06-13 14:47:19 +0300

    minor

--- NewUser.aspx.cs
+++ NewUser.aspx.cs
@@ -28,8 +28,6 @@ public partial class NewUser : System.Web.UI.Page
                
                 if (MyAdoHelperAccess.IsExist(db, sql)) //אם שם המשתמש קיים
                 {
-                    //message = "user already exists";
-                    //Response.Write("שם המשתמש קיים, בחר אחד אחר");
                     message = "User already exists.";
                     Response.AddHeader("REFRESH", "2;URL=Register.html"); //המתנה שתי שניות ומעבר לדף ההרשמה שוב
                 }
@@ -49,9 +47,7 @@ public partial class NewUser : System.Web.UI.Page
 
 
                     MyAdoHelperAccess.DoQuery("Database.mdb", creation); //בצע את השאילתא
-                    //Response.Write("תודה לך על ההרשמה, מיד תועבר/י לדף ההתחברות");
                     message = "Thanks you for registering,  you will be now redirected to the home page.";
-                    //Response.Write(" The bday is: " + Request.Form["bday"]);
                     Response.AddHeader("REFRESH", "3;URL=Main.html");
                 }
             }
